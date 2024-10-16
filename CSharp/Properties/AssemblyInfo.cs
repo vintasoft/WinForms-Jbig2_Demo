9dcd031eaff60db4b531a1be3b0790217892ab8e
vintasoft <alex@vintasoft.com>
2024-10-16 15:55:26 +0300

    Source codes update

--- CSharp/Properties/AssemblyInfo.cs
+++ CSharp/Properties/AssemblyInfo.cs
@@ -13,9 +13,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("VintaSoft JBIG2 Demo")]
 [assembly: AssemblyDescription("Shows how to create, view or modify JBIG2 files.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("VintaSoft Ltd.")]
+[assembly: AssemblyCompany("VintaSoft Llc.")]
 [assembly: AssemblyProduct("VintaSoft Imaging .NET SDK")]
-[assembly: AssemblyCopyright("Copyright VintaSoft Ltd. 2024")]
+[assembly: AssemblyCopyright("Copyright VintaSoft Llc. 2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
